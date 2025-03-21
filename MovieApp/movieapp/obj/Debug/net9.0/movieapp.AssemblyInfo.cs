8578a63454de8e788043148982533b9fe6bb1ac2
GÖRKEM-DEMİRKIRAN <gorkemgorkem1998@gmail.com>
2025-03-21 19:14:52 +0300

    Docker Created

--- MovieApp/movieapp/obj/Debug/net9.0/movieapp.AssemblyInfo.cs
+++ MovieApp/movieapp/obj/Debug/net9.0/movieapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("movieapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9321b9eae4411f388c74f44ad90da873371268e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aeac960b888b0547a25bf796e581b5d1c95a4642")]
 [assembly: System.Reflection.AssemblyProductAttribute("movieapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("movieapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 <h1 style="text-align:center;color:purple">MOVIESITE APP</h1>
 <p>Net Core file full stack a movie website has been developed.</p>
 <h3 style="color:red">Web Lınk</h3>
-<a>Click on the link</a>
+<a href="">Click on the link</a>
 <p>In this project, the TMDB API was used to create backend queries with GET requests using asynchronous methods. A database was designed to allow users to enter their own movie and TV show information and save it. Within the application, necessary information and details are retrieved based on the movies, TV shows, and genres the user wants to explore.</p>
 
 <div style="text-align:center">
